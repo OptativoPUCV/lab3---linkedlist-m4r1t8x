9bb51db78b8f6ddcf21a171053f5214af6b376a9
Your Name <you@example.com>
2024-03-27 18:01:14 +0000

    new try

--- list.c
+++ list.c
@@ -41,7 +41,7 @@ void * firstList(List * list)
 {
   if (list->head == NULL)
   {
-    Node* newNode = createNode();
+    Node* newNode = createNode(list);
     list->head = newNode;
     list->tail = newNode;
     list->current = newNode;
